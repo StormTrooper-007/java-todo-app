dd6a0cfd4031bb54ef48bb2d72a041d75050bb55
StormTrooper-007 <chigbo.c.o@gmail.com>
2023-06-26 20:38:06 +0200

    fifth version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
 
 import {
-  Box,
-
-  Button, Chip, FormControl,
-  Grid, InputLabel, MenuItem,
-  Paper, Select,
-  Stack,
-  TextField, Typography
+    Box,
+    Button, Chip, FormControl,
+    Grid, InputLabel, MenuItem,
+    Paper, Select,
+    Stack,
+    TextField, Typography
 } from "@mui/material";
 import {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import {v4 as uuidv4} from 'uuid';
-import EditIcon from "@mui/icons-material/Edit";
 import Dialog from "./components/Dialog.tsx";
+import Todo from "./components/Todo.tsx";
 
 
 export type TodoType = {
@@ -22,128 +21,135 @@ export type TodoType = {
 }
 
 function App() {
-  const [desc, setDesc] = useState<string>("");
-  const [todos, setTodos] = useState<TodoType[]>([]);
-  const [id, setId] = useState<string>("");
-  const [statusId, setStatusId] = useState<string>("");
-  const [isEdit, setIsEdit] = useState(false);
-  const [status, setStatus] = useState<string>("OPEN");
-  const todoRef = useRef("" as any);
-  const [showStatus, setShowStatus] = useState<boolean>(false);
-  const [cancelEdit, setCancelEdit] = useState(false);
+    const [desc, setDesc] = useState<string>("");
+    const [todos, setTodos] = useState<TodoType[]>([]);
+    const [id, setId] = useState<string>("");
+    const [statusId, setStatusId] = useState<string>("");
+    const [isEdit, setIsEdit] = useState<boolean>(false);
+    const [status, setStatus] = useState<string>("OPEN");
+    const todoRef = useRef("" as any);
+    const [showStatus, setShowStatus] = useState<boolean>(false);
+    const [cancelEdit, setCancelEdit] = useState<boolean>(false);
+    const [filters, setFilters] = useState<TodoType[]>([]);
+    const [activeFilters, setActiveFilters] = useState<string>("OPEN");
 
 
-  async function addTodo(){
-
-    try{
-      const newTodo:TodoType ={
-        id:uuidv4().toString(),
-        description:desc,
-        status:"OPEN"
-      }
-      await axios.post("http://localhost:8080/api/v1/todos/todo", newTodo)
-      setDesc("")
-      location.reload();
-    }catch(error){
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    async function getTodos(){
-      try {
-        const request = await axios.get("http://localhost:8080/api/v1/todos");
-        if(request){
-          setTodos(request.data);
-        }else{
-          return "could not fetch"
+    async function getTodos() {
+        try {
+            const request = await axios.get("http://localhost:8080/api/v1/todos");
+            if (request) {
+                setTodos(request.data);
+            } else {
+                return "could not fetch"
+            }
+        } catch (error) {
+            console.error(error)
         }
-      }catch(error){
-        console.error(error)
-      }
     }
-    getTodos()
-  },[])
 
-  useEffect(() => {
-    if (id !== "") {
-      setIsEdit(true);
-      todoRef.current.focus();
-    } else {
-      setIsEdit(false);
+    useEffect(() => {
+        getTodos()
+        setFilters(todos);
+    }, [])
+
+
+    async function addTodo() {
+        try {
+            const newTodo: TodoType = {
+                id: uuidv4().toString(),
+                description: desc,
+                status: "OPEN"
+            }
+            await axios.post("http://localhost:8080/api/v1/todos/todo", newTodo)
+            setDesc("")
+            location.reload();
+        } catch (error) {
+            console.log(error)
+        }
     }
-  }, [isEdit, id]);
 
-  const paperStyled = {
-    padding:2,
-    elevation:1,
-    textAlign:"center",
-    margin:3
-  }
-
-  async function deleteTodo(id:string){
-     const todoToBeDeleted = todos.find((td) => td.id.toString() === id)
-      if(todoToBeDeleted){
-        await axios.delete(`http://localhost:8080/api/v1/todos/delete?id=${id}`);
-      }
-      location.reload()
-  }
-
-
-
-  async function editTodo(desc:string, status:string, id:string){
-    try {
-      const newTodo = {
-        id: id,
-        status: status,
-        description: desc
-      }
-      const result = todos.find((todo) => todo.id.toString() === id)
-      if (result) await axios.put(`http://localhost:8080/api/v1/todos/edit?id=${id}`,
-          newTodo)
-      location.reload()
-    } catch (error) {
-      console.warn(error)
+    async function patchTodo(status: string, statusId: string) {
+        try {
+            const patchData: { status: string } = {
+                status: status
+            }
+            await axios.patch(`http://localhost:8080/api/v1/todos/status?id=${statusId}`, patchData)
+        } catch (error) {
+            console.warn(error)
+        }
     }
-  }
 
-  async function patchTodo(status: string, statusId: string) {
-    try {
-      const patchData: { status: string } = {
-        status: status
-      }
-      await axios.patch(`http://localhost:8080/api/v1/todos/status?id=${statusId}`, patchData)
-    } catch (error) {
-      console.warn(error)
+    async function editTodo(desc: string, status: string, id: string) {
+        try {
+            const newTodo = {
+                id: id,
+                status: status,
+                description: desc
+            }
+
+            await axios.put(`http://localhost:8080/api/v1/todos/edit?id=${id}`,
+                newTodo)
+            location.reload()
+        } catch (error) {
+            console.warn(error)
+        }
     }
-  }
 
-  console.log(status)
+    async function deleteTodo(id: string) {
+        try {
+            await axios.delete(`http://localhost:8080/api/v1/todos/delete?id=${id}`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        if (id !== "") {
+            setIsEdit(true);
+            todoRef.current.focus();
+        } else {
+            setIsEdit(false);
+        }
+    }, [isEdit, id]);
+
+    useEffect(() => {
+        const filters = todos.filter((td) => td.status.includes(activeFilters))
+        setFilters(filters)
+    })
+
+    const paperStyled = {
+        padding: 2,
+        elevation: 1,
+        textAlign: "center",
+        margin: 3
+    }
+
+    const activeStyled = {backgroundColor: "blue"}
 
 
   return (
       <Box sx={{flexGrow: 1}}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Paper sx={paperStyled}>Todo App</Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper sx={{display: "flex", padding: 3, justifyContent: "space-between", alignItems: "center"}}>
-              <TextField
-                  fullWidth={true}
-                  sx={{margin: 2}}
-                  multiline
-                  label="add - todo"
-                  variant="outlined"
+          <Grid container spacing={2}>
+              <Grid item xs={12} sx={{backgroundColor: "#33bfff"}}>
+                  <Paper sx={paperStyled}>Todo App</Paper>
+              </Grid>
+              <Grid item xs={12}>
+                  <Paper sx={{display: "flex", padding: 3, justifyContent: "space-between", alignItems: "center"}}>
+                      <TextField
+                          fullWidth={true}
+                          sx={{margin: 2}}
+                          multiline
+                          label="add - todo"
+                          variant="outlined"
                   inputRef={todoRef as any}
                   onChange={(event: any) => setDesc(event.target.value)}
               />
               {
                 !isEdit ?
-                    <Button sx={{marginLeft: 1, textTransform: "none"}}
+                    <Button variant="contained" sx={{marginLeft: 1, textTransform: "none"}}
                             onClick={() => addTodo()}
                     >
-                      Add Todo
+                        Add Todo
                     </Button>
                     :
                     <Button variant="contained"
@@ -193,57 +199,37 @@ function App() {
           </Box>
           <Grid  item xs={12}>
             <Paper sx={paperStyled}>
-              <Stack direction="row" spacing={1}>
-                <Chip label="Clickable" variant="outlined"/>
-                <Chip label="Clickable" variant="outlined"/>
-              </Stack>
+                <Stack direction="row" spacing={1}>
+                    <Chip onClick={() => setActiveFilters("OPEN")} label="open" variant="outlined"
+                          sx={{...activeStyled, backgroundColor: activeFilters === "OPEN" && "#349fda"}}/>
+                    <Chip onClick={() => setActiveFilters("DOING")} label="doing" variant="outlined"
+                          sx={{...activeStyled, backgroundColor: activeFilters === "DOING" && "#349fda"}}/>
+                    <Chip onClick={() => setActiveFilters("DONE")} label="done" variant="outlined"
+                          sx={{...activeStyled, backgroundColor: activeFilters === "DONE" && "#349fda"}}/>
+                </Stack>
             </Paper>
           </Grid>
 
-          <Grid container columns={{ xs: 4, sm: 8, md: 12 }}>
-            {todos.map((todo:TodoType, index:number) => (
-                  <Grid item xs={2} sm={4} md={4} key={index}>
-                    <Paper sx={paperStyled}>
-                      <Typography>Task : {todo.description}</Typography>
-                      <Typography>Task-Status : {todo.status}</Typography>
-                      {
-                        cancelEdit ?
-                            <Button onClick={() => {
-                              setCancelEdit(prev => !prev)
-                              setId("")
-                            }}
-                                    sx={{textTransform: "none"}}
-                            >cancel</Button>
-                            :
-                            <Button
-                                onClick={() => {
-                                  setId(todo.id.toString())
-                                  setCancelEdit(prev => !prev)
-                                }}>
-                              <EditIcon/>
-                            </Button>
-                      }
-
-                      {!showStatus ?
-                          <Button sx={{textTransform: "none"}} onClick={() => {
-                            setShowStatus(prev => !prev)
-                            setStatusId(todo.id.toString())
-                          }
-                          }> change status </Button>
-                          :
-                          <Button onClick={() => setShowStatus(prev => !prev)}>
-                            cancel
-                          </Button>
-                      }
-                    </Paper>
-                  </Grid>
-            ))}
-          </Grid>
-          <Grid  item xs={12}>
-            <Paper sx={paperStyled}>
-              <Typography> Footer cops</Typography>
-            </Paper>
-          </Grid>
+              <Grid container columns={{xs: 4, sm: 8, md: 12}}>
+                  {filters.map((filter: TodoType, index: number) => (
+                      <Grid item xs={2} sm={4} md={4} key={index}>
+                          <Todo setId={setId}
+                                setCancelEdit={setCancelEdit}
+                                cancelEdit={cancelEdit}
+                                setShowStatus={setShowStatus}
+                                showStatus={showStatus}
+                                setStatusId={setStatusId}
+                                filter={filter}
+                                deleteTodo={deleteTodo}
+                          />
+                      </Grid>
+                  ))}
+              </Grid>
+              <Grid item xs={12}>
+                  <Paper sx={paperStyled}>
+                      <Typography> Footer cops</Typography>
+                  </Paper>
+              </Grid>
         </Grid>
       </Box>
     )
